e6bd8777f945e740623c3e7c953a140a3c544c26
Maingron <r@maingron.com>
2026-09-19 20:42:16 +0200

    Remove Server Platform functions since they're not needed anymore

--- src/main/java/net/tenrem/doorcloser/DoorCloserPlugin.java
+++ src/main/java/net/tenrem/doorcloser/DoorCloserPlugin.java
@@ -8,8 +8,6 @@ public class DoorCloserPlugin extends JavaPlugin {
     public void onEnable() {
         Settings.ThisPlugin = this;
 
-        Settings.GetServerPlatform();
-
         Settings.ReadConfigValues();
 
         // set up commands
